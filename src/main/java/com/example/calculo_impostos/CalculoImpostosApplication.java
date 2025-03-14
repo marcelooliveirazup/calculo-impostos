760f99f1ae6390006ff78a5d52c7f3786c297c87
marcelooliveirazup <marcelo.oliveira@zup.com.br>
2025-03-14 13:43:49 -0300

    Fit: Adiciona estrutura do projeto criado no springboot.

---
+++ src/main/java/com/example/calculo_impostos/CalculoImpostosApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculo_impostos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoImpostosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoImpostosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calculo_impostos/CalculoImpostosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculo_impostos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoImpostosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
