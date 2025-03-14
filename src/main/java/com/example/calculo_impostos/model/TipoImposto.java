c95094fff553380b4e01bdb50c6bf737846e7c29
marcelooliveirazup <marcelo.oliveira@zup.com.br>
2025-03-14 15:21:48 -0300

    Fit: Adiciona entidade TipoImposto.

--- src/main/java/com/example/calculo_impostos/model/TipoImposto.java
+++ src/main/java/com/example/calculo_impostos/model/TipoImposto.java
@@ -1,4 +1,26 @@
 package com.example.calculo_impostos.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tipo_imposto")
 public class TipoImposto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private  Double aliquota; // Percentual do imposto (ex: 18.0 para 18%)
 }
+
