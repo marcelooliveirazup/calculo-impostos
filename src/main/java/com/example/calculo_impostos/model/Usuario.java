3ac240d3933cde7f6824d198a007be781af73ea9
marcelooliveirazup <marcelo.oliveira@zup.com.br>
2025-03-19 15:12:32 -0300

    Fit: Adiciona regras para colunas, userName e passWord.

--- src/main/java/com/example/calculo_impostos/model/Usuario.java
+++ src/main/java/com/example/calculo_impostos/model/Usuario.java
@@ -9,4 +9,19 @@ import lombok.*;
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String passWord;
+
+    @Enumerated(EnumType.STRING)
+    
 }
+
+
