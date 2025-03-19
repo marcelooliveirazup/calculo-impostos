366f6101cf06fe9de6d76b39fdaa73ca7f7df488
marcelooliveirazup <marcelo.oliveira@zup.com.br>
2025-03-19 14:53:10 -0300

    Fit: Adiciona importes e anotações.

--- src/main/java/com/example/calculo_impostos/model/Usuario.java
+++ src/main/java/com/example/calculo_impostos/model/Usuario.java
@@ -1,4 +1,12 @@
 package com.example.calculo_impostos.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 }
