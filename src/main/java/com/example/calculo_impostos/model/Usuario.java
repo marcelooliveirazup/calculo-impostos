b76d5f26dd34a7e8a2bf5b89acf5a327f9b03cc0
marcelooliveirazup <marcelo.oliveira@zup.com.br>
2025-03-21 15:08:12 -0300

    Refec Usuario.java.

--- src/main/java/com/example/calculo_impostos/model/Usuario.java
+++ src/main/java/com/example/calculo_impostos/model/Usuario.java
@@ -1,5 +1,5 @@
 package com.example.calculo_impostos.model;
-
+/*
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -27,4 +27,64 @@ public class Usuario {
     private Role role;
 }
 
+
+*/
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
 
