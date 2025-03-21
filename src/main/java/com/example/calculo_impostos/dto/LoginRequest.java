330fa40cd297a5cf1cc1920c5a09bf17ffd82f92
marcelooliveirazup <marcelo.oliveira@zup.com.br>
2025-03-21 15:53:34 -0300

    Fit: Adiciona LoginRequest.java com username e password.

---
+++ src/main/java/com/example/calculo_impostos/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.example.calculo_impostos.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    private String username;
+    private String password;
+}
+
