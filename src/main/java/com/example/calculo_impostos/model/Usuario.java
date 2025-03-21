bc70d9af14d2ed068c846a4876748801c9564a5c
marcelooliveirazup <marcelo.oliveira@zup.com.br>
2025-03-21 15:40:41 -0300

    Fit: Adiciona RegisterRequest com username, password e role.

--- src/main/java/com/example/calculo_impostos/model/Usuario.java
+++ src/main/java/com/example/calculo_impostos/model/Usuario.java
@@ -1,34 +1,4 @@
 package com.example.calculo_impostos.model;
-/*
-import jakarta.persistence.*;
-import lombok.*;
-
-import javax.management.relation.Role;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "usuarios")
-public class Usuario {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true)
-    private String userName;
-
-    @Column(nullable = false)
-    private String passWord;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Role role;
-}
-
-
-*/
 
 import jakarta.persistence.*;
 import lombok.*;
